a216cb7c732fbac52375de70ef78fa6dc148d454
Zhouyu Qian <qzy@qzy.io>
2017-04-08 17:51:37 -0700

    Implement usual arithmetic conversions and sizeof on type

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,20 @@
-{-# LANGUAGE RankNTypes      #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StrictData      #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE ConstraintKinds  #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes       #-}
+{-# LANGUAGE RecordWildCards  #-}
+{-# LANGUAGE StrictData       #-}
+{-# LANGUAGE TemplateHaskell  #-}
+{-# LANGUAGE TupleSections    #-}
 
 module Lib where
 
 import           Control.Lens
-import           Data.Bits
+import           Control.Monad.Except
+import           Control.Monad.Reader
+import           Data.Bits            hiding (isSigned)
 import           Data.Default
+
+-- * Type definitions
 
 data NegativeZero
   = KeepNegativeZero -- ^ 6.2.6.2/3
@@ -30,6 +37,9 @@ data CIntegralSize = CChar | CShort | CInt | CLong | CLongLong
 
 data CIntegralType = CIntegralType CIntegralSize CIntegralSignedness
   deriving (Show, Eq, Ord)
+
+data CIntegral = CIntegral CIntegralType Integer
+  deriving (Show)
 
 int :: CIntegralType
 int = CIntegralType CInt Signed
@@ -54,6 +64,7 @@ data MachineDesc
   , _longlongAddlPaddingBytes :: Int
   , _negativeRep :: NegativeRepresentation
   , _onSignedOverflow :: OnSignedIntegerOverflow
+  , _sizetSize :: CIntegralSize
   }
   deriving (Show, Eq, Ord)
 
@@ -68,15 +79,13 @@ instance Default MachineDesc where
     , _intAddlPaddingBytes = 0
     , _longAddlPaddingBytes = 0
     , _longlongAddlPaddingBytes = 0
+    , _sizetSize = CLong
     , _charSigned = True
     , _negativeRep = TwosComplement
     , _onSignedOverflow = SIOHalt
     }
 
 $(makeLenses ''MachineDesc)
-
-data CIntegral = CIntegral CIntegralType Integer
-  deriving (Show)
 
 sizeToBits :: CIntegralSize -> Getting Int MachineDesc Int
 sizeToBits CChar = charBits
@@ -96,39 +105,115 @@ validateMachineDesc :: MachineDesc -> Bool
 validateMachineDesc MachineDesc{..} =
   -- 5.2.4.2.1/1
   _charBits >= 8 && _shortBits >= 16 && _intBits >= 16 && _longBits >= 32 && _longlongBits >= 64 &&
+  -- integers with higher conversion rank must have at least as great a precision
   _charBits <= _shortBits && _shortBits <= _intBits && _intBits <= _longBits && _longBits <= _longlongBits
+  -- the sizeof type must be enough to represent sizes of integers
 
 -- | 6.3.1.1
 conversionRank :: CIntegralType -> Int
 conversionRank (CIntegralType sz _) = fromEnum sz
 
+isSigned :: MachineDesc -> CIntegralType -> Bool
+isSigned _ (CIntegralType _ Signed) = True
+isSigned _ (CIntegralType _ Unsigned) = False
+isSigned md (CIntegralType CChar Unspecified) = view charSigned md
+isSigned _ (CIntegralType _ Unspecified) = True
+
 representableRange :: MachineDesc -> CIntegralType -> (Integer, Integer)
-representableRange md (CIntegralType CChar Unspecified) =
-  representableRange md (CIntegralType CChar (if view charSigned md then Signed else Unsigned))
-representableRange md (CIntegralType sz Unspecified) = representableRange md (CIntegralType sz Signed)
-representableRange md (CIntegralType sz Unsigned) = (0, 1 `shift` view (sizeToBits sz) md - 1)
-representableRange md (CIntegralType sz Signed) =
-  case view negativeRep md of
-    TwosComplement -> (negate p, p - 1)
-    _ -> (negate p + 1, p - 1)
-  where p = 1 `shift` (view (sizeToBits sz) md - 1)
+representableRange md ty@(CIntegralType sz _) =
+  if isSigned md ty
+  then
+    let p = 1 `shift` (view (sizeToBits sz) md - 1)
+    in case view negativeRep md of
+      TwosComplement -> (negate p, p - 1)
+      _ -> (negate p + 1, p - 1)
+  else (0, 1 `shift` view (sizeToBits sz) md - 1)
 
 areAllValuesRepresentableIn :: MachineDesc -> CIntegralType -> CIntegralType -> Bool
 areAllValuesRepresentableIn md a b =
   case (representableRange md a, representableRange md b) of
     ((aMin, aMax), (bMin, bMax)) -> aMin >= bMin && aMax <= bMax
 
-integerPromotion :: MachineDesc -> CIntegral -> CIntegral
-integerPromotion md v@(CIntegral ty val)
-  | conversionRank ty < conversionRank int =
-      if areAllValuesRepresentableIn md ty int then CIntegral int val else CIntegral unsigned val
-  | otherwise = v
+-- * Evaluation and operators
 
-sizeof :: MachineDesc -> CIntegralType -> Int
-sizeof md (CIntegralType sz _) =
+data EvaluationError
+  = UndefinedBehaviorHalted
+  deriving (Show, Eq)
+
+type Ev m = (MonadReader MachineDesc m, MonadError EvaluationError m)
+
+integerPromoteType :: Ev m => CIntegralType -> m CIntegralType
+integerPromoteType ty
+  | conversionRank ty < conversionRank int =
+      asks $ \md -> if areAllValuesRepresentableIn md ty int then int else unsigned
+  | otherwise = pure ty
+
+integerPromotion :: Ev m => CIntegral -> m CIntegral
+integerPromotion (CIntegral ty val) =
+  (`CIntegral` val) <$> integerPromoteType ty
+
+integerConvertType :: Ev m => CIntegralType -> Integer -> m CIntegral
+integerConvertType ty v = do
+  md <- ask
+  if isSigned md ty then
+    case representableRange md ty of
+      (tmin, tmax) ->
+        if tmin <= v && v <= tmax then pure (CIntegral ty v)
+        else
+          case view onSignedOverflow md of
+            SIOHalt -> throwError UndefinedBehaviorHalted
+            Saturate -> pure (CIntegral ty (if v > tmax then tmax else tmin))
+            WrapAround -> undefined
+  else pure (CIntegral ty (v `mod` snd (representableRange md ty)))
+
+-- | 6.3.1.8 Returns the /common real type/. Note that this type might be
+-- different either of the types because of integer promotion rules.
+getCommonRealType :: Ev m => CIntegralType -> CIntegralType -> m CIntegralType
+getCommonRealType ty1' ty2' = do
+  md <- ask
+  ty1 <- integerPromoteType ty1'
+  ty2 <- integerPromoteType ty2'
+  case (isSigned md ty1, isSigned md ty2, conversionRank ty1, conversionRank ty2) of
+    (ty1Signed, ty2Signed, ty1Rank, ty2Rank)
+          -- Rule 1
+          | ty1 == ty2 -> pure ty1
+          -- Rule 2
+          | ty1Signed == ty2Signed && ty1Rank > ty2Rank -> pure ty1
+          | ty1Signed == ty2Signed -> pure ty2
+          -- Rule 3
+          | not ty1Signed && ty2Signed && ty1Rank >= ty2Rank -> pure ty1
+          | ty1Signed && not ty2Signed && ty2Rank >= ty1Rank -> pure ty2
+          -- Rule 4
+          | not ty1Signed && ty2Signed && areAllValuesRepresentableIn md ty1 ty2 -> pure ty2
+          | ty1Signed && not ty2Signed && areAllValuesRepresentableIn md ty2 ty1 -> pure ty1
+          -- Rule 5
+          | not ty1Signed && ty2Signed -> pure ty1
+          | ty1Signed && not ty2Signed -> pure ty2
+          | otherwise -> error "Internal error: could not perform usual arithmetic conversion"
+
+-- | 6.3.1.8 Perform the usual arithmetic conversions.
+usualArithmeticConversion :: Ev m => CIntegral -> CIntegral -> m (CIntegralType, Integer, Integer)
+usualArithmeticConversion (CIntegral ty1 v1) (CIntegral ty2 v2) = do
+  crt <- getCommonRealType ty1 ty2
+  CIntegral _ a <- integerConvertType crt v1
+  CIntegral _ b <- integerConvertType crt v2
+  pure (crt, a, b)
+
+sizeof :: Ev m => CIntegralType -> m Int
+sizeof (CIntegralType sz _) = asks $ \md ->
   let bits = view (sizeToBits sz) md
       roundedUp =
         case bits `quotRem` view charBits md of
           (b, 0) -> b
           (b, _) -> 1 + b
   in roundedUp + view (sizeToAddlPadding sz) md
+
+-- ** C Operators
+
+-- | 6.5.3.4 The C @sizeof@ operator as applied to a type. Only integral types
+-- are currently supported.
+cSizeofOnType :: Ev m => CIntegralType -> m CIntegral
+cSizeofOnType ty = do
+  v <- sizeof ty
+  sz <- view sizetSize
+  pure $ CIntegral (CIntegralType sz Unsigned) (fromIntegral v)
